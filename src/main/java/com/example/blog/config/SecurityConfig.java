99af0939048f0d757579522881d6e3f0a9ee18be
FeDLviv <fed.lviv@gmail.com>
2017-12-30 00:16:49 +0200

    Security simple work :)

--- src/main/java/com/example/blog/config/SecurityConfig.java
+++ src/main/java/com/example/blog/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.example.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,19 +16,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                //.antMatchers(HttpMethod.GET, "/api/**").authenticated()
                 .antMatchers(HttpMethod.POST, "/api/**").authenticated()
                 .antMatchers(HttpMethod.PUT, "/api/**").authenticated()
                 .antMatchers(HttpMethod.DELETE, "/api/**").authenticated()
                 .anyRequest().permitAll()
-                .and().formLogin().permitAll()
-                .and().logout().permitAll();
+                .and().httpBasic();
+        //.and().formLogin().permitAll()
+        //.and().logout().permitAll();
     }
 
-//    @Bean
-//    protected UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(User.withUsername("root").password("1111").roles("ADMIN").build());
-//        return manager;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("root").password("1111").roles("USER").build());
+        return manager;
+    }
+
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("root").password("2222").roles("USER");
 //    }
 
 }
